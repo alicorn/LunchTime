c8f42d83ebbee58ebffe60c3afe447a6d0c8d8de
Michel Streblow <michel.streblow@groupon.com>
2015-04-07 17:23:31 +0200

    changed

--- NooN/ViewController.swift
+++ NooN/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonSetting: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func button_setting(sender: AnyObject) {
+        
+        self.navigationController?.popToRootViewControllerAnimated(true)
+        
+    }
 
 }
 
